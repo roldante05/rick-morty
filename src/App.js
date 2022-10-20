1a8a96d29f19c633215ed7cc4ebf1445347b3355
Dante Roldan <danteroldan72@gmail.com>
2022-10-20 18:33:13 -0300

    retoques nuevos

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 import imagenRickMory from "./img/rick-morty.png";
 import "./App.css";
-import { useState } from "react";
 import Characters from "./components/Characters";
 
 function App() {
-  const [characters, setCharacters] = useState(null);
-
-  const reqApi = async () => {
-    const api = await fetch("https://rickandmortyapi.com/api/character");
-    
-    const characterApi = await api.json();
-    setCharacters(characterApi.results)
-
-
-   
-  };
-
   return (
     <div className="App">
       <header className="App-header">
-      
-        <h1 className="title">Rick & Morty</h1>
-        {characters ? (
-          <Characters characters={characters} setCharacters={setCharacters} />
-        ) : (
-        
-        <>
         <img src={imagenRickMory} alt="Rick0 & Morty" className="img-home" />
-        <button onClick={reqApi} className="btn-search">
-          Buscar Personajes
-        </button>
-        </>
-        )}
-        <h2 className="title" > By Dante Roldan</h2>
+        <h1 className="title">Rick & Morty</h1>
+        <Characters></Characters>
       </header>
     </div>
   );
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,54 +1,96 @@
-export default function Characters(props) {
-  const { characters, setCharacters } = props;
+import React, { useState, useEffect } from "react";
 
-  const resetCharacters = () => {
-    setCharacters(null);
+const Characters = () => {
+  // const { characters, setCharacters } = props;
+  //setear los hooks useState
+  const [characters, setCharacters] = useState([]);
+  const [search, setSearch] = useState("");
+
+  //funcion para traer los datos de la API
+
+  const api = "https://rickandmortyapi.com/api/character";
+
+  const reqApi = async () => {
+    const response = await fetch(api);
+    const dato = await response.json();
+    setCharacters(dato.results);
+    // console.log(dato.results);
   };
 
-  return (
-    <div className="characters">
-      <h1>Personajes</h1>
-      <span className="back-home" onClick={resetCharacters}>
-        {" "}
-        Volver a la home
-      </span>
-      <div className="container-characters">
-        {characters.map((character, index) => (
-          <div className="character-container" key={index}>
-            <div>
-              <img src={character.image} alt={character.name} />
-            </div>
-            <div>
-              <h3> {character.name} </h3>
+  const searcher = (e) => {
+    setSearch(e.target.value);
+    // console.log(e.target.value)
+  };
+  //metodo de filtrado
+  //funcion de busqueda
 
-              <h6>
-                {character.status === "Alive" ? (
-                  <>
-                    <span className="alive" />
-                    Alive
-                  </>
-                ) : (
-                  <>
-                    <span className="dead" />
-                    Dead
-                  </>
-                )}
-              </h6>
-              <p>
-                <span className="text-grey"> Episodios</span>
-                <span> {character.episode.length} </span>
-              </p>
-              <p>
-                <span className="text-grey"> Especie: </span>
-                <span> {character.species} </span>
-              </p>
-            </div>
-          </div>
-        ))}
+  let resultado = [];
+
+  if (!search) {
+    resultado = characters;
+  } else {
+    resultado = characters.filter((dato) =>
+      dato.name.toLowerCase().includes(search.toLocaleLowerCase())
+    );
+  }
+
+  useEffect(() => {
+    reqApi();
+  }, []);
+
+  return (
+    <div className="">
+      <div className="">
+        <input value={search} onChange={searcher} type="text" placeholder="Por ej: Rick" className="form-control"/>
       </div>
-      <span className="back-home" onClick={resetCharacters}>
-        Volver a la home
-      </span>
+
+      <div className="characters">
+        <div className="container-characters">
+          {resultado.length === 0 ? (
+            <p className="font">No hay personajes</p>
+          ) : (
+            ""
+          )}
+          {!search ? (
+            <p className=""> Busca personajes de Rick y Morty</p>
+          ) : (
+            resultado.map((character, id) => (
+              <div className="character-container" key={id}>
+                <div>
+                  <img src={character.image} alt={character.name} />
+                </div>
+                <div>
+                  <h3> {character.name} </h3>
+
+                  <h6>
+                    {character.status === "Alive" ? (
+                      <>
+                        <span className="alive" />
+                        Alive
+                      </>
+                    ) : (
+                      <>
+                        <span className="dead" />
+                        Dead
+                      </>
+                    )}
+                  </h6>
+                  <p>
+                    <span className="text-grey"> Episodios</span>
+                    <span> {character.episode.length} </span>
+                  </p>
+                  <p>
+                    <span className="text-grey"> Especie: </span>
+                    <span> {character.species} </span>
+                  </p>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Characters;
